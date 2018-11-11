9bf89bf9ae4b9647c7949a5e46aaf1b31a6fda7d
GissReyes <gissellereyes09@gmail.com>
2018-11-11 12:14:03 -0600

    Primeros Cambios
    Reestructura de niveles, htacces, entre otros

--- app/views/pages/inicio.php
+++ app/views/pages/inicio.php
@@ -1,15 +1,7 @@
  Inicio 
  <?php 
  require RUTA_APP.'\views\inc\header.php'; 
- echo $datos['titulo'];
  ?>
- <ul>
- 	<?php
- 	foreach($datos['articulos'] as $articulo) :?>
- 	<li>
- 		<?php echo $articulo->titulo; ?>
- 	</li>
- <?php endforeach; ?>
-</ul>
+ 
  <?php
  require RUTA_APP.'\views\inc\footer.php'; ?>
