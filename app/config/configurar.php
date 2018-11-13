d6c6970376f0a285e843c42ce632ad1808b8aa42
GissReyes <gissellereyes09@gmail.com>
2018-11-13 02:42:34 -0600

    Update app/config/configurar.php

--- app/config/configurar.php
+++ app/config/configurar.php
@@ -1,19 +1,19 @@
 <?php
 	//Configuración de acceso a la base de datos
-/*
+
 	define('DB_HOST','145.239.156.246');
 	define('DB_USER','labisss');
 	define('DB_PASSWORD','PonyBravo');
 	define('DB_NAME','labespecialidades');
 	define('DB_PORT','3306');
-*/
 
+/*
 	define('DB_HOST','localhost');
 	define('DB_USER','root');
 	define('DB_PASSWORD','');
 	define('DB_NAME','bloq1');
 	define('DB_PORT','3306');
-
+*/
 	//Ruta de la aplicación
 	define('RUTA_APP',dirname(dirname(__FILE__)));
 	define('RUTA_URL','http://localhost/EspecialidadesISSS-Enfermeria');
