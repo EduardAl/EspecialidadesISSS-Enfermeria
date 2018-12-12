ce53e32dc55ef3a5a56015376d45e8125a67a315
GissReyes <gissellereyes09@gmail.com>
2018-12-12 07:41:26 -0600

    Bootstrap Charts
    Archivos para gráficos

--- public/index.php
+++ public/index.php
@@ -7,19 +7,17 @@
     ?>
     <meta charset="utf-8">
     <meta name="viewport" content="initial-height=device-height width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <script type="js/bootstrap.min.js"></script>    
     <link rel="stylesheet" href="<?php echo RUTA_URL?>/css/bootstrap.min.css"> 
     <link rel="stylesheet" href="<?php echo RUTA_URL?>/css/bootstrap-theme.min.css"> 
     <link rel="stylesheet" href="<?php echo RUTA_URL?>/css/estilos.css">
     <script type="text/javascript" src="<?php echo RUTA_URL?>/js/loader.js"></script>
     <script type="text/javascript" src="<?php echo RUTA_URL?>/js/jquery.js"></script>
-    <script type="text/javascript" src ="<?php echo RUTA_URL?>/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="<?php echo RUTA_URL?>/js/bootstrap.min.js"></script>
 
     <?php
 		//Instanciamos la clase controlador
 		$Iniciar = new Core;
 	?>
-    <script type="js/jquery.js"></script>
     <script type="<?php echo RUTA_URL?>/js/jquery.js"></script>
     </body>
 </html>
